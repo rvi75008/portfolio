b7c89713d9747e09895e74169c06714e1c33e5a5
raphael vignes <rafvignes83@gmail.com>
2022-03-27 13:57:25 +0200

    chore: config, readme

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# Portfolio
+1/ Variables d'envs
+2/ faire marcher le flow
+3/ Faire une image docker 'dev' + docker compose
+4/ tester le flow
+5/ Faire une image docker 'prod' avec une vrai db et un vrai executor + docker compose
+6/ Voir pour déployer sur un cloud
---
+++ config/config.py
@@ -0,0 +1,16 @@
+from typing import Dict
+
+from pydantic import BaseSettings
+
+
+class Settings(BaseSettings):
+    STAGING_DIRECTORY: str = 'data'
+    SUCCESSFUL_INGESTION_DIR: str = 'ingested'
+    UNSUCCESSFUL_INGESTION_DIR: str = 'aborted'
+    EXTRACTION_CONFIGURATION: Dict[str, str] = {}
+    LOADER_CONNECTION_URI: str = ''
+
+    class Config:
+        case_sensitive = True
+        env_file = '.env'
+
